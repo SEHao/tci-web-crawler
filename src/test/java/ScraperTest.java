689325ee6dcd43b6ad90fd1be45c9154779a9b47
Huynh,Hao C.H <h.huynh@check6wealth.com>
2019-06-18 17:49:14 +0200

    Fix GetScrape_ReturnScrapeObject test, cause by mockito limitations of returning mock

--- src/test/java/ScraperTest.java
+++ src/test/java/ScraperTest.java
@@ -1,16 +1,16 @@
 import Models.Movie;
 import Models.Scrape;
+import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ScraperTest {
     @Test
@@ -42,62 +42,17 @@ public class ScraperTest {
                 stars
         );
 
-        Document mockDocument = mock(Document.class);
-        Element mockMediaDetailsElement = mock(Element.class);
-        Elements mockMediaDetailRowsElements = mock(Elements.class);
-        Element mockCategoryElement = mock(Element.class);
-        Element mockGenreElement = mock(Element.class);
-        Element mockFormatElement = mock(Element.class);
-        Element mockYearElement = mock(Element.class);
-        Element mockDirectorElement = mock(Element.class);
-        Element mockWritersElement = mock(Element.class);
-        Element mockStarsElement = mock(Element.class);
-
-        when(mockDocument.selectFirst("div.media-details"))
-                .thenReturn(mockMediaDetailsElement);
-        when(mockMediaDetailsElement.selectFirst("h1").text())
-                .thenReturn("The Lord of the Rings: The Fellowship of the Ring");
-        when(mockMediaDetailsElement.select("table tbody tr"))
-                .thenReturn(mockMediaDetailRowsElements);
-        when(mockCategoryElement.selectFirst("th").text())
-                .thenReturn("Category");
-        when(mockCategoryElement.selectFirst("td").text())
-                .thenReturn("Movies");
-        when(mockGenreElement.selectFirst("th").text())
-                .thenReturn("Genre");
-        when(mockGenreElement.selectFirst("td").text())
-                .thenReturn("Drama");
-        when(mockFormatElement.selectFirst("th").text())
-                .thenReturn("Format");
-        when(mockFormatElement.selectFirst("td").text())
-                .thenReturn("Blu-ray");
-        when(mockYearElement.selectFirst("th").text())
-                .thenReturn("Year");
-        when(mockYearElement.selectFirst("td").text())
-                .thenReturn("2001");
-        when(mockDirectorElement.selectFirst("th").text())
-                .thenReturn("Director");
-        when(mockDirectorElement.selectFirst("td").text())
-                .thenReturn("Peter Jackson");
-        when(mockDirectorElement.selectFirst("th").text())
-                .thenReturn("Writers");
-        when(mockDirectorElement.selectFirst("td").text())
-                .thenReturn("J.R.R. Tolkien, Fran Walsh, Philippa Boyens, Peter Jackson");
-        when(mockDirectorElement.selectFirst("th").text())
-                .thenReturn("Stars");
-        when(mockDirectorElement.selectFirst("td").text())
-                .thenReturn("Ron Livingston, Jennifer Aniston, David Herman, Ajay Naidu, Diedrich Bader, Stephen Root");
-
-        mockMediaDetailRowsElements.add(mockCategoryElement);
-        mockMediaDetailRowsElements.add(mockGenreElement);
-        mockMediaDetailRowsElements.add(mockFormatElement);
-        mockMediaDetailRowsElements.add(mockYearElement);
-        mockMediaDetailRowsElements.add(mockDirectorElement);
-        mockMediaDetailRowsElements.add(mockWritersElement);
-        mockMediaDetailRowsElements.add(mockStarsElement);
+        File htmlTemplateFile = new File("res/TestFiles/lord_of_the_rings.html");
+        Document document = null;
+        try {
+            String htmlString = FileUtils.readFileToString(htmlTemplateFile, "UTF-8");
+            document = Jsoup.parse(htmlString);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
 
         // Act
-        Scrape result = scraper.GetScrape(mockDocument);
+        Scrape result = scraper.GetScrape(document);
 
         // Assert
         assertNotNull(result);
